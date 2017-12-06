aeedc3f658f621856601243621f67b278d141528
Darren Rainey <darren@darrenraineys.co.uk>
2017-12-06 18:32:37 +0000

    Create echo.php

---
+++ PHP/echo.php
@@ -0,0 +1,8 @@
+// Code by Darren Rainey 2017 - https://GitHub.com/DarrenRainey/PHP-HTML-Example-Code
+
+// This is a comment this is not run or displayed to the user
+<?php // Start PHP function (Tell the server that this is PHP code
+
+echo 'Hello world :)'; // Prints hello world or what every you put in between the single qoutes
+
+?> // End of PHP function (Tell the server this is the end of PHP code on this page
