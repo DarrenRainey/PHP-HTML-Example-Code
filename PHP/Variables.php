897392ce921acc899c0ce55953c815ea31d23495
Darren Rainey <darren@darrenraineys.co.uk>
2017-12-10 17:02:49 +0000

    Create Variables.php

---
+++ PHP/Variables.php
@@ -0,0 +1,4 @@
+<?php // Start PHP code
+$var = 'Hello World'; // Create a variable called var and store Hello World in it.
+echo $var; // Display the contents of var with echo.
+?> // End of PHP code
